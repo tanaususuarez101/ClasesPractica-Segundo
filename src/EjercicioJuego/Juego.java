402f3cbbedd68879bcb0a8766ad49e3f4c5434e4
manupe13 <manupemen13@gmail.com>
2020-12-30 17:48:37 +0000

    Ejercicio juego

--- src/EjercicioJuego/Juego.java
+++ src/EjercicioJuego/Juego.java
@@ -1,9 +1,38 @@
 package EjercicioJuego;
 
-public class Juego {
+public class Juego implements Venta, Comparable<Juego> {
 
 	String titulo;
 	String Descripcion;
 	String plataforma;
+	
+	private int id;
+	private Double precio;
+	
+	
+	public Juego(String titulo, String descripcion, String plataforma, int id) {
+		this.titulo = titulo;
+		this.Descripcion = descripcion;
+		this.plataforma = plataforma;
+		this.id = id;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public Double getPrecio() {
+		return precio; 
+	}
+	
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	
+	public int compareTo(Juego juego) {
+		return titulo.compareTo(juego.titulo) ;
+	}
+	
 	
 }
---
+++ src/EjercicioJuego/Tienda.java
@@ -0,0 +1,52 @@
+package EjercicioJuego;
+
+import java.util.*;
+
+import Main.Finca;
+
+public class Tienda {
+	
+	ArrayList<Juego> estanteria; 
+	private int orden;
+	
+	
+	public Tienda() {
+		this.estanteria = new ArrayList<Juego>(); 
+	}
+	
+	public boolean add(Juego juego) {
+		if(!estanteria.contains(juego)) {
+			estanteria.add(juego);
+			return true;
+		}
+		return false;
+	}
+	
+	public void setOrder(int id) {
+		this.orden = id;
+		
+	}
+	
+	public List<Juego> getList(){	
+		if(this.orden == 0) {
+			Collections.sort(estanteria, new Comparator<Juego>() {
+				@Override
+				public int compare(Juego j1, Juego j2) {
+					return j1.compareTo(j2);
+				}
+			});
+			
+		} else {
+			Collections.sort(estanteria, new Comparator<Juego>() {
+				@Override
+				public int compare(Juego j1, Juego j2) {
+					return j2.compareTo(j1);
+				}
+			});
+		}
+		return estanteria;
+	}
+	
+	
+	
+}
